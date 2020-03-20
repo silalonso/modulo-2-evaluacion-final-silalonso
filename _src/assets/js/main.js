95a6536cea0c2650c8a5458819a5822a83cfc171
silvia <silvia.alonso.pozas@gmail.com>
2020-03-20 03:04:33 +0100

    Gets api data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+let listSeries = [];
+let favoriteSeries = [];
+// los pongo vacíos porque luego se pintarán
+
+let inputSearch = document.querySelector("js-input-search");
+const buttonSearch = document.querySelector("js-button-search");
+let resultSearch = document.querySelector("js-results-search");
+
+function getApiData(ev) {
+  ev.preventDefault();
+  let text = inputSearch.nodeValue;
+  fetch(`//api.tvmaze.com/search/shows?q=${text}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      listSeries = data;
+      paintlistSeries();
+    });
+}
+buttonSearch.addEventListener("click", getApiData);
